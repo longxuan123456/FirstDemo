fabac1d3dfa70e1127d683a1cefdcd55de1905c7
longxuan123456 <34728594+longxuan123456@users.noreply.github.com>
2020-05-20 15:09:32 +0800

    IDE编程工具，第一次使用JAVA代码

--- GitHubDemo/src/com/gitHub/Demo/FirstDemo.java
+++ GitHubDemo/src/com/gitHub/Demo/FirstDemo.java
@@ -5,6 +5,10 @@ package com.gitHub.Demo;
  * @author long
  * */
 public class FirstDemo {
+    public static void main(String[] args) {
 
-    
+        System.out.println("Hello, World!");
+
+    }
+
 }
